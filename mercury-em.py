bad07940e4ae21073d0fcf28be9831af911bc481
h8axstella <166030410+h8axstella@users.noreply.github.com>
2024-04-11 12:08:05 +0300

    Update mercury-em.py

--- mercury-em.py
+++ mercury-em.py
@@ -21,7 +21,7 @@ def parse_cmd_line_args():
 
     parser.add_argument("--proto", choices=["m206", "m236"], nargs='?', default="m206",
                         help='Mercury protocol (M206/M236)')
-    parser.add_argument('--serial', type=int, nargs='?', default=0, help='Device serial number', required=True)
+    parser.add_argument('--serial', type=str, nargs='?', default=0, help='Device serial number', required=True)
 
     parser.add_argument('--host', type=str, nargs='?', default=0, help='RS485-TCP/IP Convertor IP.')
     parser.add_argument('--port', type=int, nargs='?', default="50", help='RS485-TCP/IP Convertor (default: 50)')
@@ -36,7 +36,7 @@ def parse_cmd_line_args():
                         choices=[0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x09, 0x0A, 0x0B, 0x0C, 0x0D],
                         type=int,
                         nargs='?',
-                        default=0x00,
+                        default=0x0B,
                         help=('Номер массива\n'
                               '  0  - От сброса (по умолчанию)\n'
                               '  1  - За текущий год\n'
@@ -73,71 +73,58 @@ def print_output(arr, output_format):
 
 if __name__ == "__main__":
     args = parse_cmd_line_args()
+    serial_numbers = [int(serial) for serial in args.serial.split(',')]
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((args.host, args.port))
-
-    result = {}
-
-    if args.proto == "m206":
-        ''' Сетевой адрес счетчика - серийный номер 
-        '''
-        result['info'] = {}
+    for serial in serial_numbers:  # Добавлено двоеточие
         try:
-            result['info']['V'], result['info']['A'], result['info']['P'] = mercury206.read_vap(sock, args.serial)
-            result['info']['freq'] = mercury206.read_freq(sock, args.serial)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(5)
+            sock.connect((args.host, args.port))
+            result = {}
 
-            result['energy'] = mercury206.read_energy(sock, args.serial)
-        except TimeoutError:
-            result['error'] = "Timeout while read data from socket"
-        except ValueError:
-            result['error'] = "Wrong data"
+            if args.proto == "m206":
+                result['info'] = {}
+                try:
+                    result['info']['V'], result['info']['A'], result['info']['P'] = mercury206.read_vap(sock, serial)
+                    result['info']['freq'] = mercury206.read_freq(sock, serial)
 
+                    result['energy'] = mercury206.read_energy(sock, serial)
+                except TimeoutError:
+                    result['error'] = "Timeout while reading data from socket"
+                except ValueError:
+                    result['error'] = "Wrong data"
+
+            elif args.proto == "m236":
+                serial_mod = serial % 1000  
+
+                if serial_mod == 0:
+                    serial_mod = 1
+                elif serial_mod > 240:
+                    serial_mod = serial_mod % 100
+
+                user_level = 0x02 if args.user == "admin" else 0x01
+                passwd = args.passwd or ("222222" if user_level == 0x02 else "111111")
+
+                try:
+                    mercury236.check_connect(sock, serial_mod)
+                    mercury236.open_channel(sock, serial_mod, user_level, passwd)
+
+                    result[f'energy_phases_{args.array_number}'] = mercury236.read_energy_sum_act_react(sock, serial_mod, param=0x0B)
+                    result[f'energy_tarif_{args.array_number}'] = mercury236.read_energy_tarif_act_react(sock, serial_mod, param=0x0B)
+                    result['energy_phases'] = mercury236.read_energy_sum_by_phases(sock, serial_mod)
+                    result['energy_tarif'] = mercury236.read_energy_tarif_by_phases(sock, serial_mod)
+
+                    result['info'] = mercury236.read_vap(sock, serial_mod)
+                    result['info']['freq'] = mercury236.read_freq(sock, serial_mod)
+
+                    mercury236.close_channel(sock, serial_mod)
+                except TimeoutError:
+                    result['error'] = "Timeout while reading data from socket"
+                except ValueError:
+                    result['error'] = "Wrong data"
+
+            print_output(result, args.format)
+        except Exception as e:
+            print(f"Ошибка: {e}")
         finally:
-            sock.close()
-
-    elif args.proto == "m236":
-
-        ''' Сетевым адресом счетчика по умолчанию являются три последние цифры заводского номера или две
-            последние цифры в случае, если три последние цифры образуют число более 240.
-            Если три последние цифры – нули, то сетевой адрес "1". 
-        '''
-        args.serial = args.serial % 1000
-
-        if args.serial == 0:
-            args.serial = 1
-        elif args.serial > 240:
-            args.serial = args.serial % 100
-
-        ''' уровень доступа "User"  - 0x01 default passwd = 111111
-            уровень доступа "Admin" - 0x02 default passwd = 222222
-        '''
-
-        args.user = 0x02 if args.user == "admin" else 0x01
-        if not args.passwd:
-            args.passwd = "222222" if args.user == 2 else "111111"
-
-        try:
-            mercury236.check_connect(sock, args.serial)
-            mercury236.open_channel(sock, args.serial, args.user, args.passwd)
-
-            result[f'energy_phases_{args.array_number}'] = mercury236.read_energy_sum_act_react(sock, args.serial,
-                                                                                                param=args.array_number)
-            result[f'energy_tarif_{args.array_number}'] = mercury236.read_energy_tarif_act_react(sock, args.serial,
-                                                                                                 param=args.array_number)
-            result['energy_phases'] = mercury236.read_energy_sum_by_phases(sock, args.serial)
-            result['energy_tarif'] = mercury236.read_energy_tarif_by_phases(sock, args.serial)
-
-            result['info'] = mercury236.read_vap(sock, args.serial)
-            result['info']['freq'] = mercury236.read_freq(sock, args.serial)
-
-            mercury236.close_channel(sock, args.serial)
-        except TimeoutError:
-            result['error'] = "Timeout while read data from socket"
-        except ValueError:
-            result['error'] = "Wrong data"
-
-        finally:
-            sock.close()
-
-    print_output(result, args.format)
+            sock.close()  
